c0da0de6e5fef3935ada41b4ce9817f1381cc11e
JDugganQuinnipiac <jduggan@quinnipiac.edu>
2023-02-07 20:03:47 -0500

    Prompt user to input 2 whole numbers to be added together

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 public class Calculator{
 
     public static void main(String[] args) {
-        
+        System.out.println("Please enter 2 whole numbers");
+
+        Scanner reader = new Scanner(System.in);
+        int x = reader.nextInt();
+        int y = reader.nextInt();
+        reader.close();
+
+        System.out.println("The sum of your numbers is: " + (x + y));
     }
 }
