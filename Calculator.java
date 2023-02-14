e3cb8481910ee86557c3b43bc0cdf1433524fba0
JDugganQuinnipiac <jduggan@quinnipiac.edu>
2023-02-14 13:40:22 -0500

    added Multiplication and Division options

--- Calculator.java
+++ Calculator.java
@@ -8,14 +8,19 @@ public class Calculator{
         int x = reader.nextInt();
         int y = reader.nextInt();
 
-        System.out.println("Would you like to add or subrtract these numbers? (Type '1' for Addition or '2' for Subtraction)");
-        int AddOrSub = reader.nextInt();
+        System.out.println("Would you like to add, subrtract, multiply, or divide these numbers? (Type '1' for Addition, '2' for Subtraction, '3' for Multiplication, or '4' for Division)");
+        int operation = reader.nextInt();
         reader.close();
 
-        if(AddOrSub == 1){
+        if(operation == 1){
             System.out.println("The sum of your numbers is " + (x + y));
-        }else if(AddOrSub == 2){
+        }else if(operation == 2){
             System.out.println("The difference of your numbers is " + (x - y));
+        }else if(operation == 3){
+            System.out.println("The product of your numbers is " + (x * y));
+        }else if(operation == 4){
+            double quotient = (double)x/y;
+            System.out.println("The quotient of your numbers is " + quotient);
         }else{
             System.out.println("Sorry, invalid reponse. Please Try again.");
         }
