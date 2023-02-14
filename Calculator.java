d8bb87a8f7ebf7ba2738781d20ca15bff64c1b28
JDugganQuinnipiac <jduggan@quinnipiac.edu>
2023-02-14 13:23:26 -0500

    allow user to choose if they want to add or subtract their numbers

--- Calculator.java
+++ Calculator.java
@@ -7,8 +7,17 @@ public class Calculator{
         Scanner reader = new Scanner(System.in);
         int x = reader.nextInt();
         int y = reader.nextInt();
+
+        System.out.println("Would you like to add or subrtract these numbers? (Type '1' for Addition or '2' for Subtraction)");
+        int AddOrSub = reader.nextInt();
         reader.close();
 
-        System.out.println("The sum of your numbers is: " + (x + y));
+        if(AddOrSub == 1){
+            System.out.println("The sum of your numbers is " + (x + y));
+        }else if(AddOrSub == 2){
+            System.out.println("The difference of your numbers is " + (x - y));
+        }else{
+            System.out.println("Sorry, invalid reponse. Please Try again.");
+        }
     }
 }
