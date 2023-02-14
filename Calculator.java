56722db44b27a4577dc5a5a0ddc5db82def01b1b
JDugganQuinnipiac <jduggan@quinnipiac.edu>
2023-02-14 13:49:22 -0500

    added support for floating point numbers

--- Calculator.java
+++ Calculator.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class Calculator{
 
     public static void main(String[] args) {
-        System.out.println("Please enter 2 whole numbers");
+        System.out.println("Please enter 2 numbers (Decimals allowed)");
 
         Scanner reader = new Scanner(System.in);
-        int x = reader.nextInt();
-        int y = reader.nextInt();
+        float x = reader.nextFloat();
+        float y = reader.nextFloat();
 
         System.out.println("Would you like to add, subrtract, multiply, or divide these numbers? (Type '1' for Addition, '2' for Subtraction, '3' for Multiplication, or '4' for Division)");
         int operation = reader.nextInt();
@@ -19,8 +19,7 @@ public class Calculator{
         }else if(operation == 3){
             System.out.println("The product of your numbers is " + (x * y));
         }else if(operation == 4){
-            double quotient = (double)x/y;
-            System.out.println("The quotient of your numbers is " + quotient);
+            System.out.println("The quotient of your numbers is " + (x / y));
         }else{
             System.out.println("Sorry, invalid reponse. Please Try again.");
         }
